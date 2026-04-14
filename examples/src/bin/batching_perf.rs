84dd3f00c694d638dc1d1e4d5ace0475e978cd08
Alexandre-Fromantin <alexandre.fromantin08@gmail.com>
2026-04-14 19:39:33 +0200

    update batching examples

---
+++ examples/src/bin/batching_perf.rs
@@ -0,0 +1,63 @@
+use std::{path::Path, time::Instant};
+
+use jeys_hash_set::{BatchingParameter, HashSet};
+use tokio::{fs::remove_file, io};
+use tracing::{Level, info};
+use tracing_subscriber::FmtSubscriber;
+
+const DEGREE: u8 = 20;
+const NB_INSERT: u32 = 2u32.pow(DEGREE as u32 + 3);
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::TRACE)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+
+    for batching_size_i in 7..17 {
+        remove_file("data/data.bin").await;
+        remove_file("data/journal.bin").await;
+
+        let batching_size: u32 = 2u32.pow(batching_size_i);
+
+        let batching_param = BatchingParameter {
+            pre_allocated_size: batching_size as usize,
+        };
+
+        let mut hash_set: HashSet = HashSet::new(Path::new("data"), DEGREE, batching_param)
+            .await
+            .unwrap();
+
+        let mut data = Vec::with_capacity(batching_size as usize);
+
+        let time = Instant::now();
+        for i in 0..(NB_INSERT / batching_size) {
+            data.clear();
+            for y in 0..batching_size {
+                data.push(i as u64 * batching_size as u64 + y as u64);
+            }
+            hash_set.batch_insert(&data).await;
+        }
+        let elapsed = time.elapsed();
+        info!(
+            "for batching size: {} | {} insertion time: {:?} | {:?}/insert",
+            batching_size,
+            NB_INSERT,
+            elapsed,
+            elapsed / NB_INSERT
+        );
+        println!("");
+
+        drop(hash_set);
+
+        let time = Instant::now();
+        let hash_set2: HashSet = HashSet::from_file(Path::new("data"), batching_param)
+            .await
+            .unwrap();
+        info!("recovery time {:?}", time.elapsed());
+        println!("");
+    }
+
+    Ok(())
+}
